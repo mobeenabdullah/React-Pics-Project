3d1bde22f42bd20483854760284ebf09649f7ad8
Mobeen Abdullah <mobeenabdullah@gmail.com>
2020-07-30 01:50:07 +0500

    added axios from npm

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar'
 
 class App extends React.Component {
+
+    onSearchSubmit = term => {
+        console.log(term);
+    }
+
     render() {
         return (
             <div className="ui container" >
-                <SearchBar />
+                <SearchBar onSubmit={this.onSearchSubmit} />
             </div>
         );
     }
